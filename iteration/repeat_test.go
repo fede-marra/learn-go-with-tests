20a3d26ed8765d3362fc4aa83fefc851cefb6d8f
fede-marra <fedemarra@gmail.com>
2023-03-24 16:50:37 -0300

    Se agrego la posibilidad de decidir mediante variable que caracter repetir y de la misma manera la cantidad de veces que se puede repetir. Ademas se modifico el codigo en repeat.go para darle funcionalidad para cumplir dicho test

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,14 +1,23 @@
 package iteration
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	char := "y"
+	cant := 5
+	repeated := Repeat(char, cant)
+	expected := strings.Repeat(char, cant)
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
+
+//func BenchmarkRepeat(b *testing.B) {
+//	for i := 0; i < b.N; i++ {
+//		Repeat((char,cant))
+//	}
+//}
