fe6544e5d58c3cc22809cecf2fa4ff88e527fa37
fede-marra <fedemarra@gmail.com>
2023-03-28 15:32:21 -0300

    last commit of the slice

--- arrays-and-slices/slice/sum_test.go
+++ arrays-and-slices/slice/sum_test.go
@@ -1,29 +1,27 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestSum(t *testing.T) {
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
 
-		got := Sum(numbers)
-		want := 15
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
 
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
 
-		got := Sum(numbers)
-		want := 6
-
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 }
